3928df5e7708e7c9bc39c8f51819c44a1782fbb1
Amir Ghafouri <14092997+amirghafouri@users.noreply.github.com>
2020-05-28 20:09:51 +0800

    Updates

---
+++ precache-manifest.f1e4436fe5170f0750ed52776270cf61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860313a0a31daca48632acb9d0746b95",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a2e306b27813a4b9e265",
+    "url": "/monsters-rolodex/static/css/main.220c6ecd.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2e306b27813a4b9e265",
+    "url": "/monsters-rolodex/static/js/main.04b72810.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
